c1e4373e483b8110007fb977cb64edea6818b87a
Vinícius Leal <viniciusleal.lc@gmail.com>
2022-07-19 03:20:50 -0300

    Primeiro commit

---
+++ Para Erros/ajustes.sql
@@ -0,0 +1,4 @@
+-- Se aparecer a seguiunte mensagem de erro;
+--yarn.ps1 não pode ser carregado porque a execução de scripts foi desabilitada neste sistema. 
+--Executar o seguinte comando no windows: Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+
